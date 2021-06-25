653102a033333726d63d5cc20a2e6aaa83adc597
Tom Benyon <tom@benyon.io>
2021-06-25 17:01:10 +0100

    Data addition for free

--- src/utilities/pricingStringBuilder.js
+++ src/utilities/pricingStringBuilder.js
@@ -2,8 +2,7 @@
   REQUIREMENTS
 
   The price should be displayed in the view.
-  If the price is 0 it should say free.
-  If the product is free it should say the word "FREE"
+  If the product price is 0 it should say the word "FREE"
   If it's a recurring price the price should say "per month/year"
   If it's discounted we should have a before and saving price "Was £234. Now £123"
   If the product is free we should show a banner saying "free"
